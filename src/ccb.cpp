68d99d2677a4c8311b0bf9760f1ecaf9105a1fde
Dimitrije Pešić <dimitrije.pesicc@gmail.com>
2025-10-02 21:15:37 +0200

    Refactor CCB class and improve coroutine management

--- src/ccb.cpp
+++ src/ccb.cpp
@@ -2,75 +2,49 @@
 // Created by os on 9/26/25.
 //
 
-#ifndef PROJECT_BASE_V1_1_CCB_HPP
-#define PROJECT_BASE_V1_1_CCB_HPP
+#include "../h/ccb.hpp"
+#include "../h/riscv.hpp"
+#include "../h/Scheduler.hpp"
+#include "../h/syscall_c.hpp"
 
-#include "../lib/hw.h"
-#include "Scheduler.hpp"
+CCB *CCB::running = nullptr;
+CCB* CCB::mainThread = nullptr;
 
-class CCB {
-public:
+void CCB::yield() {
+    thread_dispatch();
+}
 
-    ~CCB() {delete[] stack;}
-
-    bool isFinished() const { return finished; }
-
-    void setFinished(bool finished) {
-        CCB::finished = finished;
+void CCB::dispatch() {
+    CCB *old = running;
+    if (old != CCB::mainThread && !old->isFinished()) {
+        Scheduler::put(old);
     }
 
-    using Body = void(*)(void*);
+    running = Scheduler::get();
+    if (!running) running = CCB::mainThread;
 
-    static CCB *createCoroutine(Body body, void* arg);
+    if (old != running) {
+        contextSwitch(old->getContext(), running->getContext());
+    }
+}
 
+CCB* CCB::createCoroutine(Body body, void* arg) {
+    CCB* ccb = new CCB(body, arg);
+    if (ccb && body) {
+        Scheduler::put(ccb);
+    }
+    return ccb;
+}
 
-    static void yield();
+void CCB::coroutineWrapper() {
+    // Uključi prekide, nastavi normalno
+    Riscv::ms_sstatus(Riscv::SSTATUS_SIE);
 
-    static CCB *running;
-    static CCB* mainThread;
-
-    static void dispatch();
-
-    time_t getSleepTime() const { return sleepTime; }
-
-    void setSleepTime(time_t time) { sleepTime = time; }
-
-    struct Context
-    {
-        uint64 ra; // Return address
-        uint64 sp; // Stack pointer
-    };
-    static void contextSwitch(Context *oldContext, Context *runningContext);
-
-    Context* getContext() { return &context; }
-
-private:
-
-    time_t sleepTime = 0;
-
-    static void coroutineWrapper();
-
-    explicit CCB(Body body, void* arg) :
-            body(body),
-            arg(arg),
-            stack(body != nullptr ? new uint64[STACK_SIZE] : nullptr),
-            context({
-                            body != nullptr ? (uint64)&coroutineWrapper : 0,
-                            stack != nullptr ? (uint64)&stack[STACK_SIZE] : 0
-                    }),
-            finished(false)
-    {
-        if (body != nullptr) Scheduler::put(this);
+    if (running->body) {
+        running->body(running->arg);
     }
 
-    Body body;
-    void* arg;
-    uint64 *stack;
-    Context context;
-    bool finished;
+    running->setFinished(true);
+    thread_exit(); // ne vraća se
+}
 
-    static uint64 constexpr STACK_SIZE = 1024;
-};
-
-
-#endif //PROJECT_BASE_V1_1_CCB_HPP
