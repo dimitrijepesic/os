98ffef52651817a979cef980cae621231ac0c32e
Dimitrije Pešić <dimitrije.pesicc@gmail.com>
2025-10-02 20:36:07 +0200

    Updated MemoryAlloc

--- h/MemoryAllocator.hpp
+++ h/MemoryAllocator.hpp
@@ -1,4 +1,6 @@
-// h/MemoryAllocator.hpp
+//
+// Created by os on 9/24/25.
+//
 
 #ifndef PROJECT_BASE_V1_1_MEMORYALLOCATOR_HPP
 #define PROJECT_BASE_V1_1_MEMORYALLOCATOR_HPP
@@ -7,32 +9,39 @@
 
 class MemoryAllocator {
 public:
+    // --- Glavni javni interfejs ---
     static void init();
     static void* mem_alloc (size_t size);
     static int mem_free (void* p);
 
-    // Header of each segment in the heap
+    // --- Dodatne javne funkcije prema specifikaciji ---
+    static uint64 getFreeSpaceBytes();
+    static uint64 getLargestFreeBlockBytes();
+    static uint64 sizeToBlocks(size_t bytes);
+
+    // Header (zaglavlje) svakog segmenta u heap-u
     struct memBlock{
         size_t sizeBlocks;
         bool isFree;
-        memBlock* next = nullptr;
-        memBlock* prev = nullptr;
+        memBlock* next;
+        memBlock* prev;
     };
 
-    static size_t sizeToBlocks(size_t bytes);
-
 private:
+
+    // Statički podaci
     static memBlock* freeHead;
     static uint64 heapStart;
     static uint64 heapEnd;
 
-    // Helper functions
+    // Pomoćne (helper) funkcije
     static memBlock* userHeader(void* userPtr);
     static void* headerUser(memBlock* h);
     static void insertSorted(memBlock* blk);
     static void removeFree(memBlock* blk);
-    static void split(memBlock* blk, uint64 needBlocks);
+    static void split(memBlock* blk, size_t needBlocks);
     static void tryCoalesce(memBlock* blk);
 };
+
 
 #endif //PROJECT_BASE_V1_1_MEMORYALLOCATOR_HPP
